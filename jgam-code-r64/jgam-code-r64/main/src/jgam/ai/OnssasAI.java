db29f0bf39886b8a4119f652c45dcc0fd7b0b2e3
BadenP <ptowersp@hotmail.com>
2024-06-17 21:13:01 -0300

    tentativa de mover logo as pecas distantes

--- jgam-code-r64/jgam-code-r64/main/src/jgam/ai/OnssasAI.java
+++ jgam-code-r64/jgam-code-r64/main/src/jgam/ai/OnssasAI.java
@@ -32,38 +32,57 @@ public class OnssasAI implements AI {
         int opponent = 3 - player;
 
         int checkersInLastSix = 0;
+        int checkersInFirstSix = 0;
+
         for (int i = 1; i < 25; i++) {
             int numCheckers = bs.getPoint(player, i);
             int opponentNumCheckers = bs.getPoint(opponent, i);
 
             if (opponentNumCheckers == 1)
-                eval += 75.0;
+                eval += 80.0;
             else if (opponentNumCheckers == 0)
-                eval += 25.0;
+                eval += 40.0;
             else
-                eval -= 100.0;
+                eval -= 150.0;
             
-            if (numCheckers >= 3)
-                eval -= 10.0 * numCheckers;
-            else if (numCheckers == 2)
-                eval += 100.0;
+            if (numCheckers >= 4)
+                eval -= 30.0 * numCheckers;
+            else if (numCheckers >= 2)
+                eval += 200.0;
             else if (numCheckers == 1)
-                eval -= 75.0;
+                eval -= 250.0;
 
             if ((player == 1 && i >= 19) || (player == 2 && i <= 6))
                 checkersInLastSix += numCheckers;
+            
+            if ((player == 1 && i <= 6) || (player == 2 && i >= 19))
+                checkersInFirstSix += numCheckers;
+
+            
+            if ((player == 1 && i <= 13) || (player == 2 && i >= 12))
+                eval -= 20.0 * numCheckers;
+            else 
+                eval += 20.0 * numCheckers;
         }
 
         if (checkersInLastSix == 15)
             eval += 1000.0;
         else if (checkersInLastSix >= 12)
-            eval += 100.0;
+            eval += 400.0;
         else if (checkersInLastSix >= 9)
-            eval += 50.0;
+            eval += 250.0;
         else if (checkersInLastSix >= 6)
-            eval += 20.0;
+            eval += 40.0;
         else if (checkersInLastSix >= 3)
-            eval += 10.0;
+            eval += 15.0;
+
+        if (checkersInFirstSix == 0)
+            eval += 10000.0;
+        else if (checkersInFirstSix == 1)
+            eval -= 500.0;
+        else if (checkersInFirstSix >= 2)
+            eval -= 20000.0;
+        
 
         int totalPoints = bs.getPoint(player, 25);
         eval += 50.0 * totalPoints;
