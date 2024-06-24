f54956986aaf0d7dfbc9b78aa723237f17fb9ab1
Luca <lucafidideus@gmail.com>
2024-06-23 22:20:46 -0300

    panel adicionado

--- JGammon/src/jgam/Version.java
+++ JGammon/src/jgam/Version.java
@@ -2,7 +2,7 @@
 			package jgam;
 			public class Version {
 				public static final String VERSION = "1.1-pre2";
-				public static final String BUILD_NUMBER = "20";
-				public static final String BUILD_DATE = "Tue, 18 Jun 2024 23:39:15 -0300";
+				public static final String BUILD_NUMBER = "34";
+				public static final String BUILD_DATE = "Sun, 23 Jun 2024 21:56:34 -0300";
 			}
 		
