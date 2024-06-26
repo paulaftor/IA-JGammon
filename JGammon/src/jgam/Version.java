87cdb0c7a2e032d75d462f86582fd1d04bc7be58
BadenP <ptowersp@hotmail.com>
2024-06-25 21:48:21 -0300

    minimax aaa

--- JGammon/src/jgam/Version.java
+++ JGammon/src/jgam/Version.java
@@ -2,7 +2,7 @@
 			package jgam;
 			public class Version {
 				public static final String VERSION = "1.1-pre2";
-				public static final String BUILD_NUMBER = "40";
-				public static final String BUILD_DATE = "Sun, 23 Jun 2024 23:33:01 -0300";
+				public static final String BUILD_NUMBER = "84";
+				public static final String BUILD_DATE = "Tue, 25 Jun 2024 21:28:04 -0300";
 			}
 		
