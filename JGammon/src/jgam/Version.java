08257589e47fe9b71c0741f8757902392fffef7a
BadenP <ra123565@uem.br>
2024-06-27 22:13:53 -0300

    recursivo

--- JGammon/src/jgam/Version.java
+++ JGammon/src/jgam/Version.java
@@ -2,7 +2,7 @@
 			package jgam;
 			public class Version {
 				public static final String VERSION = "1.1-pre2";
-				public static final String BUILD_NUMBER = "122";
-				public static final String BUILD_DATE = "Wed, 26 Jun 2024 00:33:25 -0300";
+				public static final String BUILD_NUMBER = "164";
+				public static final String BUILD_DATE = "Thu, 27 Jun 2024 10:54:15 -0300";
 			}
 		
